88cb78ddf8b64a28b7af12a9d44591ea83a5deab
Luiz Gustavo Véras <gustavo_veras@ifsp.edu.br>
2023-11-09 19:08:45 -0300

    Ajustes código camera

--- aulas/aula8_inter_dispo/flutter_camera/lib/main.dart
+++ aulas/aula8_inter_dispo/flutter_camera/lib/main.dart
@@ -1,16 +1,18 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Retorna o objeto do tipo List<CameraDescription>
   final cameras = await availableCameras();
-  final firstCamera = cameras.first;
+  final firstCamera = cameras.first; // retorna a primeira câmera disponível
 
   runApp(MaterialApp(
     home: TakePicture(
@@ -20,17 +22,18 @@ void main() async {
 }
 
 class TakePicture extends StatefulWidget {
+  // CameraDescription é o tipo de objeto que representa uma câmera
   final CameraDescription camera;
   TakePicture({required this.camera});
 
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _TakePictureState();
   }
 }
 
 class _TakePictureState extends State<TakePicture> {
+  // CameraController obtem a imagem de uma câmera
   late final CameraController _controller;
   late final Future<void> _initController;
 
@@ -38,17 +41,21 @@ class _TakePictureState extends State<TakePicture> {
   void initState() {
     super.initState();
     _controller = CameraController(
-      widget
-          .camera, // widget representa o state. O campo camera foi definido em TakePicture
-      ResolutionPreset.max, // resolução da câmera
+      // widget representa o campo da classe TakePicture.
+      widget.camera, // O campo câmera foi definido em TakePicture
+      ResolutionPreset.max, // resolução da câmera setada para máxima
     );
 
+    // Dá início ao processo de se conectar com a camera.
+    // Não se sabe quando a conexão com a camera será finalizada,
+    // logo, initialize() retorna uma Future.
     _initController = _controller.initialize();
   }
 
   @override
+  // É invocado quando o Widget for removido da árvore de Widgets
   void dispose() {
-    _controller.dispose();
+    _controller.dispose(); // Libera a câmera.
     super.dispose();
   }
 
@@ -58,14 +65,20 @@ class _TakePictureState extends State<TakePicture> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
+          // Precisamos utilizar o FutureBuilder, pois não se sabe quando a
+          // câmera será conectada.
           FutureBuilder<void>(
-            future: _initController,
+            future:
+                _initController, // Future que aguarda a conexão com a câmera.
             builder: (context, snapshot) {
+              // Se a conexão com a câmera tiver sido estabelecida
               if (snapshot.connectionState == ConnectionState.done) {
                 return Expanded(
                   child: Center(
                     child: AspectRatio(
                       aspectRatio: _controller.value.aspectRatio,
+                      // CameraPreview é um Widget da biblioteca "camera" que
+                      // permite visualizar a imagem da camera em tempo real.
                       child: CameraPreview(_controller),
                     ),
                   ),
@@ -77,11 +90,23 @@ class _TakePictureState extends State<TakePicture> {
               );
             },
           ),
-          IconButton(
-            onPressed: () {
-              _takePhoto(context);
-            },
-            icon: Icon(Icons.photo_camera),
+
+          // Botão para salvar a imagem da camera.
+
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 5,
+              ),
+            ),
+            margin: EdgeInsets.all(10),
+            child: IconButton(
+              onPressed: () {
+                _takePhoto(context);
+              },
+              color: Colors.red,
+              icon: Icon(Icons.photo_camera),
+            ),
           )
         ],
       ),
@@ -89,24 +114,31 @@ class _TakePictureState extends State<TakePicture> {
   }
 
   void _takePhoto(BuildContext context) async {
-    // garante que o controller está pronto
+    // Garante que o controller da câmera esteja pronto quando uma foto for tirada.
     await _initController;
 
     // Nome do arquivo e caminho
-    //final dir = await getTemporaryDirectory(); // função do pacote path_provider
-    final dir = await getExternalStorageDirectory() ?? Directory("/");
+    // final dir = await getTemporaryDirectory(); // função do pacote path_provider
+    // final dir = await getExternalStorageDirectory() ?? Directory("/");
+    final dir = await getDownloadsDirectory() ?? Directory("/");
     final name =
         "img_${DateTime.now()}.png"; // usando a data no nome, evita-se conflito de nomes de arquivos
 
     // Armazena a foto em uma dado local
     final fullPath = path.join(dir.path, name); // path é
     final image = await _controller.takePicture();
-    print(fullPath);
-    image.saveTo(fullPath);
+    // Salva a foto
+    // image.saveTo(fullPath);
+    Uint8List byteImage = await image.readAsBytes();
+    ImageGallerySaver.saveImage(byteImage);
 
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      content: Text("Picture taken!"),
-      duration: Duration(milliseconds: 600),
-    ));
+    String texto = "Foto salva em $fullPath";
+    // Snackback para apresentar que a foto foi salva
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("Imagem salva"),
+        duration: Duration(milliseconds: 2000),
+      ),
+    );
   }
 }
